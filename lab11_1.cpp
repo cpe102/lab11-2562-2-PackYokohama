282b616c61fa6abc84e0d406c54f0b7057d652b9
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-02-04 17:09:32 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 //Write prototype of function fibonacci() here
+long long int fibonacci(int);
+
 
 int main(){
 	cout << fibonacci(0) << "\n";
@@ -17,8 +19,13 @@ int main(){
 
 
 //Write definition of function fibonacci() here
-/*
-??? fibonacci(???){
-  
+long long int fibonacci(int x){
+	if(x==0) return 0;
+	if(x==1) return 1;
+	else 
+	return fibonacci(x-1) + fibonacci(x-2);
 }
-*/
+
+	
+  
+
