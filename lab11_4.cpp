c4df516b68b38921d5b50ada8072623c7f77c394
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-02-04 17:10:36 +0700

    Update lab11_4.cpp

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -3,8 +3,11 @@
 
 using namespace std;
 
-//Declare prototype of function template mySwap() here
 
+
+//Declare prototype of function template mySwap() here
+template <typename T>
+void mySwap(T &,T &);
 
 int main(){
 	
@@ -33,7 +36,12 @@ int main(){
 
 	return 0;
 }
+template <typename T>
+void mySwap(T &x,T &y)
+{
+T z;
+z=x;
+x=y;
+y=z;
 
-
-//Write definition of function template mySwap() here
-
+}
